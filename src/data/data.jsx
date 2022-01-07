443e0b4f7bda913a9ef8ea93e5b897e441ddf0e4
Adriano Ortiz Garda <adriano.ortizgarda@gmail.com>
2022-01-07 13:33:01 -0600

    Nuevas fotos añadidas

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -19,6 +19,27 @@ import Dieciocho from "../images/proyectosPage/Hoteles.png"
 import Diecinueve from "../images/proyectosPage/Jardines.png"
 import Veinte from "../images/proyectosPage/Restaurantes.png"
 import Veinte1 from "../images/proyectosPage/Terrazas.png"
+import NuevaUno from "../images/nuevasFotos/NuevaUno.jpg"
+import NuevaDos from "../images/nuevasFotos/NuevaDos.jpg"
+import NuevaTres from "../images/nuevasFotos/NuevaTres.jpg"
+import NuevaCuatro from "../images/nuevasFotos/NuevaCuatro.jpg"
+import NuevaCinco from "../images/nuevasFotos/NuevaCinco.jpg"
+import NuevaSeis from "../images/nuevasFotos/NuevaSeis.jpg"
+import NuevaSiete from "../images/nuevasFotos/NuevaSiete.jpg"
+import NuevaNueve from "../images/nuevasFotos/NuevaNueve.jpg"
+import NuevaDiez from "../images/nuevasFotos/NuevaDiez.jpg"
+import NuevaOnce from "../images/nuevasFotos/NuevaOnce.jpg"
+import NuevaDoce from "../images/nuevasFotos/NuevaDoce.png"
+import NuevaTrece from "../images/nuevasFotos/NuevaTrece.jpg"
+import NuevaCatorce from "../images/nuevasFotos/NuevaCatorce.jpg"
+import NuevaQuince from "../images/nuevasFotos/NuevaQuince.jpg"
+import NuevaDieciseis from "../images/nuevasFotos/NuevaDieciseis.png"
+import NuevaDiecisiete from "../images/nuevasFotos/NuevaDiecisiete.jpg"
+import NuevaDieciocho from "../images/nuevasFotos/NuevaDieciocho.jpg"
+import NuevaDiecinueve from "../images/nuevasFotos/NuevaDiecinueve.jpg"
+import NuevaVeinte from "../images/nuevasFotos/NuevaVeinte.jpg"
+import NuevaVeintiuno from "../images/nuevasFotos/NuevaUno.jpg"
+
 
 export const IMAGES = [
     {
@@ -124,6 +145,106 @@ export const IMAGES = [
     {
       id: 21,
       src: Veinte1,
+      alt: "Placeholder image",
+    },
+    {
+      id: 22,
+      src: NuevaUno,
+      alt: "Placeholder image",
+    },
+    {
+      id: 23,
+      src: NuevaDos,
+      alt: "Placeholder image",
+    },
+    {
+      id: 24,
+      src: NuevaTres,
+      alt: "Placeholder image",
+    },
+    {
+      id: 25,
+      src: NuevaCuatro,
+      alt: "Placeholder image",
+    },
+    {
+      id: 26,
+      src: NuevaCinco,
+      alt: "Placeholder image",
+    },
+    {
+      id: 27,
+      src: NuevaSeis,
+      alt: "Placeholder image",
+    },
+    {
+      id: 28,
+      src: NuevaSiete,
+      alt: "Placeholder image",
+    },
+    {
+      id: 29,
+      src: NuevaNueve,
+      alt: "Placeholder image",
+    },
+    {
+      id: 30,
+      src: NuevaDiez,
+      alt: "Placeholder image",
+    },
+    {
+      id: 31,
+      src: NuevaOnce,
+      alt: "Placeholder image",
+    },
+    {
+      id: 32,
+      src: NuevaDoce,
+      alt: "Placeholder image",
+    },
+    {
+      id: 33,
+      src: NuevaTrece,
+      alt: "Placeholder image",
+    },
+    {
+      id: 34,
+      src: NuevaCatorce,
+      alt: "Placeholder image",
+    },
+    {
+      id: 35,
+      src: NuevaQuince,
+      alt: "Placeholder image",
+    },
+    {
+      id: 36,
+      src: NuevaDieciseis,
+      alt: "Placeholder image",
+    },
+    {
+      id: 37,
+      src: NuevaDiecisiete,
+      alt: "Placeholder image",
+    },
+    {
+      id: 38,
+      src: NuevaDieciocho,
+      alt: "Placeholder image",
+    },
+    {
+      id: 39,
+      src: NuevaDiecinueve,
+      alt: "Placeholder image",
+    },
+    {
+      id: 40,
+      src: NuevaVeinte,
+      alt: "Placeholder image",
+    },
+    {
+      id: 41,
+      src: NuevaVeintiuno,
       alt: "Placeholder image",
     },
   ];
